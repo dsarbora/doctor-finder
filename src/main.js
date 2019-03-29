5e97480617fa3d3ffd6ed9e815f75e201c7dd09d
Dave Sarbora <dsarbora@gmail.com>
2019-03-29 16:39:39 -0700

    get names and info showing from api call

--- src/main.js
+++ src/main.js
@@ -12,7 +12,15 @@ $(document).ready(function() {
         format: 'json'
       },
       success: function(response) {
-        $('#name').text(`${response.data.practices}`);
+        if(response.data.length==0){
+          $('#name').text("No results found.");
+        }
+        else{
+          $('#name').text(`${response.data[0].profile.first_name} ${response.data[0].profile.last_name}`);
+          $('#address').text(`${response.data[0].practices[0].visit_address.street}`);
+          $('#phone').text(`${response.data[0].practices[0].phones[0].number}`);
+          $('#website').text(`${response.data[0].practices[0].website}`);
+        }
       },
       error: function() {
         $('#errors').text("There was an error processing your request. Please try again.");

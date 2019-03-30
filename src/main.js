98378baae08c96992d885790c45fb4992d646004
Dave Sarbora <dsarbora@gmail.com>
2019-03-29 19:48:36 -0700

    allow multiple doctors to show at once

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ $(document).ready(function() {
   $('#sicknessButton').click(function() {
     let sickness = $('#sickness').val();
     $('#sickness').val("");
-    console.log(sickness);
     $.ajax({
       url: `https://api.betterdoctor.com/2016-03-01/doctors?query=${sickness}&location=wa-seattle&skip=0&limit=10&user_key=${process.env.exports.apiKey}`,
       type: 'GET',
@@ -16,10 +15,14 @@ $(document).ready(function() {
           $('#name').text("No results found.");
         }
         else{
-          $('#name').text(`${response.data[0].profile.first_name} ${response.data[0].profile.last_name}`);
-          $('#address').text(`${response.data[0].practices[0].visit_address.street}`);
-          $('#phone').text(`${response.data[0].practices[0].phones[0].number}`);
-          $('#website').text(`${response.data[0].practices[0].website}`);
+          $('#result').text("");
+          for(var i=0; i<10; i++){
+            $('#result').append(`<h4>Name: ${response.data[i].profile.first_name} ${response.data[i].profile.last_name}</h4>
+                <h4>Address: ${response.data[0].practices[i].visit_address.street}</h4>
+                <h4>Phone: ${response.data[0].practices[i].phones[0].number}
+                <h4>Website: ${response.data[0].practices[i].website}
+                <hr>`);
+          }
         }
       },
       error: function() {

f22d7814c45575fe9ec82ee6e6fbb7b349aa7b2e
Dave Sarbora <dsarbora@gmail.com>
2019-04-01 07:29:08 -0700

    provide additional README instructions

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## Doctor Lookup
 #### By Dave Sarbora
-#### To help you find the right doctor for your ailment!
+#### To help you search the right doctor!
 ## Description
 
 This application will take in your city and ailment, and search a list of doctors who can help you with a particular problem.
@@ -14,8 +14,40 @@ If the query response doesn't include any doctors (for instance, if no doctors m
 
 ## Setup/Installation Requirements
 * Download the respository from [https://github.com/dsarbora/doctor-finder](https://github.com/dsarbora/doctor-finder)
+* _Download and install node.js and Node Package Manager._
+* _Navigate to the project directory in your command line._
+* _Use the command $ **npm install**_
+* _Use a web browser to navigate to []() and create an account to generate your own API key that will work with this program.
+* _Create a directory titled ".env" at the root level of this project, and in it include the line "exports.apiKey = {Copy and paste your API key here.}
+* _When necessary dependencies are finished loading, use the command $ **npm run build**_
+* _Finally, use the command $ **npm run start** to start the web server._
+
+
 ## Known Bugs
+* Whether or not the information appearing on the screen actually represents an accurate depiction of reality is unknown to the author of this program.
+* **response.data[i].practices** is an array, and when a doctor works at more than one location, data may only reflect one location's information.
+* The accepting new patients section appears to be drawn from the practice as opposed to the doctor. 
 
 ## Technologies Used
+* Atom
+* JavaScript
+* HTML
+* CSS
+* Node.js
+* Node Package Manager
+* Webpack
+* UglifyJs
+* ESLint
+* DotEnv
 
 #### License
+
+*MIT
+
+Copyright (c) 2019 by *_Dave Sarbora_*
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- src/main.js
+++ src/main.js
@@ -1,35 +1,47 @@
 import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
 $(document).ready(function() {
   $('#sicknessButton').click(function() {
     let sickness = $('#sickness').val();
     $('#sickness').val("");
-    $.ajax({
-      url: `https://api.betterdoctor.com/2016-03-01/doctors?query=${sickness}&location=wa-seattle&skip=0&limit=10&user_key=${process.env.exports.apiKey}`,
-      type: 'GET',
-      data: {
-        format: 'json'
-      },
-      success: function(response) {
-        if(response.data.length==0){
-          $('#name').text("No results found.");
-        }
-        else{
-          $('#result').text("");
-          for(var i=1; i<10; i++){
-            $('#result').append(`<h2>Doctor ${i}</h2><h4>Name: ${response.data[i-1].profile.first_name} ${response.data[i-1].profile.last_name}</h4>
-                <h4>Address: ${response.data[i-1].practices[0].visit_address.street}</h4>
-                <h4>Phone: ${response.data[i-1].practices[0].phones[0].number}
-                <h4>Website: ${response.data[i-1].practices[0].website}</h4>
-                <h4>Accepting New Patients: ${response.data[0].practices[0].accepts_new_patients}</h4>
-                <hr>`);
-          }
-        }
-      },
-      error: function() {
-        $('#errors').text("There was an error processing your request. Please try again.");
+    let request = new XMLHttpRequest();
+    const url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${sickness}&location=or-portland&limit=10&user_key=${process.env.exports.apiKey}`;
+    request.onreadystatechange = function(){
+      if(this.readyState===4 && this.status===200){
+        const response = JSON.parse(this.responseText);
+        getElements(response);
       }
-    });
+    };
+
+    request.open("GET", url, true);
+    request.send();
+
+    const getElements = function(response){
+      if(response.data.length==0){
+        $('#result').text("No results found.");
+      }
+      else{
+        $('#result').text("");
+        for(var i=1; i<11; i++){
+          let acceptingNew = response.data[i-1].practices[0].accepts_new_patients;
+          let acceptingNewEmoji='';
+          if(acceptingNew){
+            acceptingNewEmoji='👍';
+          }
+          else{
+            acceptingNewEmoji='👎';
+          }
+          $('#result').append(`<h2>Doctor ${i}</h2><h4>Name: ${response.data[i-1].profile.first_name} ${response.data[i-1].profile.last_name}</h4>
+          <h4>Address: ${response.data[i-1].practices[0].visit_address.street}</h4>
+          <h4>Phone: ${response.data[i-1].practices[0].phones[0].number}
+          <h4>Website: ${response.data[i-1].practices[0].website}</h4>
+          <h4>Accepting New Patients: ${acceptingNewEmoji}</h4>
+          <hr>`);
+        }
+      }
+    };
   });
 });
